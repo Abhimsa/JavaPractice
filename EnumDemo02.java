0f7ee276850830e80d1e9a5d79aa4872d7ee7f74
Abhishek Maguluri <abhishekmaguluri@gmail.com>
2025-06-01 03:17:03 +0530

    Third commit in JP

---
+++ EnumDemo02.java
@@ -0,0 +1,12 @@
+//package Enumerations;
+
+enum Weekdays {
+    Sunday, Monday, Tuesday
+}
+
+public class EnumDemo02 {
+    public static void main(String[] args) {
+        Weekdays c1 = Weekdays.Tuesday;
+        System.out.println(c1);
+    }
+}
